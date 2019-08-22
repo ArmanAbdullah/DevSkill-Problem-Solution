bdeb0ad9d491c394f39c1a63772de9975794ca3c
creation <armanabdullah101@gmail.com>
2019-08-22 22:23:44 +0600

    DCP-147 is added in Easy.

---
+++ Easy/DCP-147.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class DCP_147
+    {
+        static void Main(String[] args)
+        {
+            String input;
+            while ((input = Console.ReadLine())!=null)
+            {
+                ulong n = Convert.ToUInt64(input);
+                ulong i, j, k;
+                ulong count =0;
+                for (i = 3,j=5,k=15; i< n; i += 3,j+=5,k+=15)
+                {
+                    if(j < n)
+                    {
+                        count += j;
+                    }
+                    count += i;
+                    if (k < n)
+                    {
+                        count -= k;
+                    }
+                    
+
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
